30b45b025a72b1a6c15f45b312585ce1d94a45ba
Andrew Chavarria <andrew.chavarria77@gmail.com>
2022-11-10 17:52:59 -0500

    Adds PreviousGamesActivity files

---
+++ app/src/main/java/com/example/discgolfpracticeaid/PreviousGamesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.discgolfpracticeaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class PreviousGamesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_previous_game_page)
+
+    }
+}
