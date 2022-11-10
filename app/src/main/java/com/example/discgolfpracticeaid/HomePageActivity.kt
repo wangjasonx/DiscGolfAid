cfc0f858d5fc2723d73f7a51a65399d741a6023b
mlisch11 <mlisch11@terpmail.umd.edu>
2022-11-10 17:57:50 -0500

    redid new game page and began hole fragment

--- app/src/main/java/com/example/discgolfpracticeaid/HomePageActivity.kt
+++ app/src/main/java/com/example/discgolfpracticeaid/HomePageActivity.kt
@@ -2,10 +2,23 @@ package com.example.discgolfpracticeaid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import com.example.discgolfpracticeaid.databinding.ActivityHomePageBinding
+
+private lateinit var binding: ActivityHomePageBinding
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.newGame.setOnClickListener {
+            val NewGameActivity = Intent(this, NewGameActivity::class.java)
+            startActivity(NewGameActivity)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/discgolfpracticeaid/NewGameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.discgolfpracticeaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_game)
+    }
+}
